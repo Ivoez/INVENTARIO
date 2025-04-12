9e9430b3093ee0276d6a8a8e2a696051ef832bcc
fran <franciscopieri123@gmail.com>
2025-04-12 09:04:33 -0300

    Muevo la carpeta UniversidadBase a mi carpeta person

--- UniversidadBase/app/views/layout/footer.php
+++
@@ -1,7 +0,0 @@
-<footer class="bg-light text-center py-3 mt-5">
-        <p>&copy; <?php echo date('Y'); ?> - <?php echo NOMBRE_SITIO; ?></p>
-    </footer>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-</body>
-</html>
