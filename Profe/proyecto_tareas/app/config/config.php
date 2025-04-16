098a4b6b07b0a7d3b960e8d74d56eace1d0af50f
juanPabloCesarini <juanpcesarini@hotmail.com>
2025-04-16 14:36:48 -0300

    proyecto terminado -muestra-

---
+++ Profe/proyecto_tareas/app/config/config.php
@@ -0,0 +1,17 @@
+<?php 
+   // configuracion acceso a la BD
+   define('DB_HOST','localhost');
+   define('DB_USER','root');
+   define('DB_PASSWORD','');
+   define('DB_NAME','base_tareas');
+
+   // Ruta de la aplicación
+   define('RUTA_APP', dirname(dirname(__FILE__)));
+   // Ruta url
+
+   define('RUTA_URL','http://localhost/APPWEB_CABA_1C_2025/Profe/proyecto_tareas');
+
+   // Rutas que se usan para guardar imágenes
+   define('RUTA_AVATAR','/APPWEB_1C_2025/Profe/proyecto_tareas/public/img/avatar/');
+   define('NOMBRESITIO','Administrador de tareas');
+?>
