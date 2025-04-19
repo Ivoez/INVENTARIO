9c18b7d8e077a58ac6bf280faa194e97a343dc84
Nahuel <nahuelagustinflorentin1@hotmail.com>
2025-04-19 11:37:36 -0300

    MVC ACTUALIZADO

---
+++ INVENTARIO/app/config/config.php
@@ -0,0 +1,14 @@
+<?php 
+   // configuracion acceso a la BD
+   define('DB_HOST','localhost');
+   define('DB_USER','root');
+   define('DB_PASSWORD','');
+   define('DB_NAME','Aca la base de datos');
+
+   // Ruta de la aplicación
+   define('RUTA_APP', dirname(dirname(__FILE__)));
+   // Ruta url
+
+   define('RUTA_URL','http://localhost/appweb_caba_1c_2025/INVENTARIO');
+   define('NOMBRESITIO','FRAMEWORK PHP - MVC');
+?>
