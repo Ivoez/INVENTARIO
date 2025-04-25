548d54f814be9f293e4facb8bfdb99c8b1477c53
BenjaminH <benjaguitars@gmail.com>
2025-04-25 18:14:48 -0300

    Creacion stylecss, carpeta img, agregado logo, actualizacion header

--- GYM/app/views/layout/header.php
+++ GYM/app/views/layout/header.php
@@ -5,11 +5,38 @@
   <meta name="viewport" content="width=device-width, initial-scale=1" />
   <title>GYM</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" />
+  <link rel="stylesheet" href="public/css/style.css" />
 </head>
 <body>
-  <nav class="navbar navbar-dark bg-dark mb-4">
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark shadow-sm mb-4">
     <div class="container">
-      <a class="navbar-brand" href="#">Sistema GYM</a>
+      <a class="navbar-brand d-flex align-items-center" href="#">
+        <img src="public/img/logogym.png" alt="Logo GYM" />
+        <span>Sistema GYM</span>
+      </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarGYM" aria-controls="navbarGYM" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      
+      <div class="collapse navbar-collapse" id="navbarGYM">
+        <ul class="navbar-nav ms-auto me-2">
+          <li class="nav-item">
+            <a class="nav-link active" href="#">Inicio</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Socios</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Actividades</a>
+          </li>
+        </ul>
+        <a href="#" class="btn btn-success">Registrarse</a>
+      </div>
     </div>
   </nav>
-  <div class="container">
+
+  
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
