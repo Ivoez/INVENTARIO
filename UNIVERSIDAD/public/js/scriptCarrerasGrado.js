1a2a5487ffbb71ce34bc4de0e591298e98d69d6d
Ariel-Nuñez-11 <tu-ariel_cris2112@hotmail.com>
2025-05-01 16:49:47 -0300

    git commit -m "Imagenes carrusel"

---
+++ UNIVERSIDAD/public/js/scriptCarrerasGrado.js
@@ -0,0 +1,45 @@
+
+const postGradoCourses = [
+    {
+        id: 'carrera1',
+        title: 'Ingenieria de Sistemas',
+        image: '../public/img/IngenieriaDeSistemas.jpg',
+        thumb: '../public/img/IngenieriaDeSistemas.jpg',
+        description: 'La Ingeniería en Sistemas se dedica al diseño, implementación y gestión de sistemas informáticos. Los ingenieros en sistemas desarrollan software y soluciones tecnológicas, asegurando que los sistemas funcionen de manera eficiente y satisfagan las necesidades de los usuarios y las organizaciones.'
+    },
+    {
+        id: 'carrera2',
+        title: 'Desarrollo de Software',
+        image: '../public/img/DesarrolloDeSoftware.jpg',
+        thumb: '../public/img/DesarrolloDeSoftware.jpg',
+        description: 'El desarrollo de software es el proceso de creación, diseño, implementación y mantenimiento de programas informáticos. Implica una serie de actividades que van desde la conceptualización de una idea hasta la entrega de un producto funcional y probado. '
+    },
+    {
+        id: 'carrera3',
+        title: 'Tecnologia De la Informacion',
+        image: '../public/img/TecnologiaDeLaInformacion.jpeg',
+        thumb: '../public/img/TecnologiaDeLaInformacion.jpeg',
+        description: 'La Tecnología de la Información (TI) se refiere al uso de herramientas y tecnologías para crear, almacenar, proteger, y compartir información digital. Esto incluye hardware, software, redes, y los procesos necesarios para gestionar la información de manera eficiente.'
+    },
+    {
+        id: 'carrera4',
+        title: 'Gestión de Proyectos Tecnológicos',
+        image: '../public/img/GestionProyectosTecnologicos.jpg',
+        thumb: '../public/img/GestionProyectosTecnologicos.jpg',
+        description: 'La gestión de proyectos tecnológicos implica planificar, ejecutar, supervisar y controlar proyectos que involucran tecnología, ya sea software, hardware, infraestructura o aplicaciones. Esto incluye desde la implementación de nuevos sistemas informáticos hasta el desarrollo de soluciones innovadoras. '
+    }
+    {
+        id: 'carrera4',
+        title: 'Gestión de Proyectos Tecnológicos',
+        image: '../public/img/GestionProyectosTecnologicos.jpg',
+        thumb: '../public/img/GestionProyectosTecnologicos.jpg',
+        description: 'La gestión de proyectos tecnológicos implica planificar, ejecutar, supervisar y controlar proyectos que involucran tecnología, ya sea software, hardware, infraestructura o aplicaciones. Esto incluye desde la implementación de nuevos sistemas informáticos hasta el desarrollo de soluciones innovadoras. '
+    }
+    {
+        id: 'carrera4',
+        title: 'Gestión de Proyectos Tecnológicos',
+        image: '../public/img/GestionProyectosTecnologicos.jpg',
+        thumb: '../public/img/GestionProyectosTecnologicos.jpg',
+        description: 'La gestión de proyectos tecnológicos implica planificar, ejecutar, supervisar y controlar proyectos que involucran tecnología, ya sea software, hardware, infraestructura o aplicaciones. Esto incluye desde la implementación de nuevos sistemas informáticos hasta el desarrollo de soluciones innovadoras. '
+    }
+  ];
