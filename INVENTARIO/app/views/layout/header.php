414bdb1cb2b6c9dacaeb34e56b9cde99a9239339
aalanguerra <galan2736@gmail.com>
2025-05-02 19:56:43 -0300

    cambio de icono

--- INVENTARIO/app/views/layout/header.php
+++ INVENTARIO/app/views/layout/header.php
@@ -4,6 +4,7 @@
    <meta charset="UTF-8">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <link rel="stylesheet" href="<?php echo RUTA_URL?>/css/style.css">
+   <link rel="shortcut icon" type="image/x-icon" href="Imagenes/InventarioIco.ico"
 
 
    <!--ENLACE A JS-->
