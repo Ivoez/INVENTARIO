9637819f9e71239d3d92c36fbb59d854ba1de55f
maxigrinstein <maxigrinstein@gmail.com>
2025-05-03 13:14:07 -0300

    creacion header separado para la pagina de autenticación

--- GYM/app/views/pages/register.php
+++ GYM/app/views/pages/register.php
@@ -27,16 +27,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 
 ?>
-
-<!DOCTYPE html>
-<html lang="es">
-<head>
-<meta charset="UTF-8">
-<title>Login / Register</title>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-<link rel="stylesheet" href="<?php echo RUTA_URL; ?>/public/css/styleregister.css">
-</head>
-<body>
+<?php require RUTA_APP . '/views/layout/auth/header.php'; ?>
 
 <input type="radio" name="panel" id="sign-in-toggle" checked hidden>
 <input type="radio" name="panel" id="sign-up-toggle" hidden>
