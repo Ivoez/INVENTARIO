5063b97d7ca8c695a33dc9585c530876052950ff
rantegas <santinooliveto1@hotmail.com>
2025-05-16 15:38:39 -0300

    Creacion de paginas de recuperacion de contraseña + estilos

---
+++ GYM/app/views/pages/auth/forgotPassword.php
@@ -0,0 +1,17 @@
+<?php require RUTA_APP . '/views/layout/auth/headerForgotPassword.php'; ?>
+
+<body>
+    <div class="container">
+        <div class="form-container">
+            <form action="#">
+                <h1 class="text-center">RECUPERAR CONTRASEÑA</h1>
+                <span class="text-center">Ingresa tu nueva contraseña para reestablecerla.</span>
+                <input type="password" placeholder="Nueva contraseña" required>
+                <input type="password" placeholder="Repetir nueva contraseña" required>
+                <button>
+                    <span>ENVIAR!</span>
+                </button>
+            </form>
+        </div>
+    </div>
+</body>
---
+++ GYM/app/views/pages/auth/recoverPassword.php
@@ -0,0 +1,16 @@
+<?php require RUTA_APP . '/views/layout/auth/headerForgotPassword.php'; ?>
+
+<body>
+    <div class="container">
+        <div class="form-container">
+            <form action="#">
+                <h1 class="text-center">RECUPERAR CONTRASEÑA</h1>
+                <span class="text-center">Ingresa tu mail para recibir un enlace y recuperar tu contraseña.</span>
+                <input type="email" placeholder="Email" required>
+                <button>
+                    <span>ENVIAR!</span>
+                </button>
+            </form>
+        </div>
+    </div>
+</body>
