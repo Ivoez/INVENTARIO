6df288c4b8a88a83b3286765e252c2b2c027651d
maxigrinstein <maxigrinstein@gmail.com>
2025-05-24 09:34:44 -0300

    Creacion modelo profesores y comienzo dashboard crud profesores

--- GYM/app/controllers/DashboardController.php
+++ GYM/app/controllers/DashboardController.php
@@ -3,7 +3,7 @@ require_once __DIR__ . '/../core/BaseController.php';
 
 class DashboardController extends BaseController {
     public function index() {
-        echo "Dashboard";
+        
         require_once __DIR__ . '/../views/admin/dashboard.php';
     }
 }
