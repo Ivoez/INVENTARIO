45dec09ca8dcc59e31f0e74940f1787e5cd8a731
Marcela <marcelamt.tr@gmail.com>
2025-05-24 00:47:03 -0300

    Nuevo index

--- INVENTARIO/app/views/pages/Register.php
+++ INVENTARIO/app/views/pages/Register.php
@@ -1,24 +1,15 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="Register.css"> <!--carpeta de estilos-->
-    <title>Registro de Usuarios</title>
-</head>
-<body>
-    <div class="container-body">
-        <div class="ctn-form">
+
+<link rel="stylesheet" href="<?php echo RUTA_URL?>public/css/style.css">
+<?php require RUTA_APP .'/views/layout/header.php';?>
+
+
+
+<title>Registro de Usuarios</title>
+
+
+
+<div class="container-body">
+    <div class="ctn-form">
             <img src="imagenes/Icono_simple.png" alt="usuario-login"> <!--agrega la ubicacion de la imagen de usuario-->
             <h1 class="title">Formulario de Registro de Usuario Nuevo</h1>
         <form action="">
@@ -37,6 +28,7 @@
             
         </form>
 </div>
-    
-</body>
-</html>
+
+
+
+<?php require RUTA_APP .'/views/layout/footer.php';?>
